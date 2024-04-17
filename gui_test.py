ca675144e7e8de780424e42aee92b5bc9546582e
Marco Chrappan Soldavini <magyar1886@gmail.com>
2024-04-18 01:17:10 +0200

    Update gui_test.py

--- gui_test.py
+++ gui_test.py
@@ -6,22 +6,87 @@ import threading
 import time
 from opcua import ua
 
+# logging setting
 logging.basicConfig(level=logging.WARN)
 
 event = threading.Event()
+pu_status = 0
+mes_status = 0
 
-def thread_function(name, client_instance):
+# thread running in parallel to read pu_status
+def thread_function(name, client_instance, mes_statuses, pu_statuses, batch_id_entry, recipe_id_entry):
+
+    old_pu_status = 0
+    old_mes_status = 0
 
     while 1:
-        logging.warning("Thread %s: starting", name)
-        time.sleep(2)
+        logging.warning("Thread %s: running", name)
+        time.sleep(1)
 
         if event.is_set():
             break
 
-        print(client_instance.client.get_node("ns=4;i=6").get_value())
+        # read opc value for pu_status and mes_status
+        global pu_status
+        pu_status = client_instance.client.get_node("ns=4;i=7").get_value()
+        global mes_status
+        mes_status = client_instance.client.get_node("ns=4;i=6").get_value()
 
-# fun definitions
+        batch_id_text = batch_id_entry.get()
+        recipe_id_text = recipe_id_entry.get()
+
+        if mes_status == 2 and pu_status == 2:
+            client_instance.client.get_node("ns=4;i=14").set_value(ua.DataValue(ua.Variant(batch_id_text, ua.VariantType.String)))
+            client_instance.client.get_node("ns=4;i=15").set_value(ua.DataValue(ua.Variant(recipe_id_text, ua.VariantType.String)))
+
+        # if old value is different from actual value it resets all label text to black
+        if pu_status != old_pu_status:
+            for i in range(0,7):
+                pu_statuses[i].config(fg="black")
+                old_pu_status = pu_status
+
+        # setting label text color to match pu_status value
+        match pu_status:
+            case 0:
+                pu_statuses[0].config(fg="red")
+            case 1:
+                pu_statuses[1].config(fg="red")
+            case 2:
+                pu_statuses[2].config(fg="red")
+            case 3:
+                pu_statuses[3].config(fg="red")
+            case 4:
+                pu_statuses[4].config(fg="red")
+            case 5:
+                pu_statuses[5].config(fg="red")
+            case 8:
+                pu_statuses[6].config(fg="red")
+
+        # if old value is different from actual value it resets all label text to black
+        if mes_status != old_mes_status:
+            for i in range(0,7):
+                mes_statuses[i].config(fg="black")
+                old_mes_status = mes_status
+
+        # setting label text color to match mes_status value
+        match mes_status:
+            case 0:
+                mes_statuses[0].config(fg="red")
+            case 1:
+                mes_statuses[1].config(fg="red")
+            case 2:
+                mes_statuses[2].config(fg="red")
+            case 3:
+                mes_statuses[3].config(fg="red")
+            case 4:
+                mes_statuses[4].config(fg="red")
+            case 5:
+                mes_statuses[5].config(fg="red")
+            case 8:
+                mes_statuses[6].config(fg="red")
+
+
+# client class to manage opc tag writes, connection and disconnection
 class connected_client:
 
     def __init__(self, address, port):
@@ -48,13 +113,15 @@ class connected_client:
 
     def setMESStatus(self,value):
         self.client.get_node("ns=4;i=6").set_value(ua.DataValue(ua.Variant(value, ua.VariantType.Int16)))
+        global mes_status
+        mes_status = value
 
 
 # OPC CLIENT
 pu_client = connected_client("192.168.8.166","4840")
 
-# thread function
-x = threading.Thread(target=thread_function, args=(1,pu_client))
+
+###################################################### GUI START #############################################################
 
 # app frame
 window = tk.Tk()
@@ -74,13 +141,10 @@ mes_status4 = tk.Label(master=window, text="4 - Ready to start")
 mes_status5 = tk.Label(master=window, text="5 - Batch data reading finished")
 mes_status8 = tk.Label(master=window, text="8 - N/A")
 
-mes_status0.grid(row=5, column=0, padx=5, pady=5)
-mes_status1.grid(row=6, column=0, padx=5, pady=5)
-mes_status2.grid(row=7, column=0, padx=5, pady=5)
-mes_status3.grid(row=8, column=0, padx=5, pady=5)
-mes_status4.grid(row=9, column=0, padx=5, pady=5)
-mes_status5.grid(row=10, column=0, padx=5, pady=5)
-mes_status8.grid(row=11, column=0, padx=5, pady=5)
+mes_statuses = [mes_status0, mes_status1, mes_status2, mes_status3, mes_status4, mes_status5, mes_status8]
+
+for i in range(0,7):
+    mes_statuses[i].grid(row=5+i, column=0, padx=5, pady=5)
 
 # buttons MES
 button_0 = tk.Button(master=window, text="Set 0", command=lambda: pu_client.setMESStatus(0))
@@ -108,30 +172,32 @@ pu_status4 = tk.Label(window, text="4 - Batch active")
 pu_status5 = tk.Label(window, text="5 - Batch finished")
 pu_status8 = tk.Label(window, text="8 - Error")
 
-pu_status0.grid(row=5, column=2, padx=5, pady=5)
-pu_status1.grid(row=6, column=2, padx=5, pady=5)
-pu_status2.grid(row=7, column=2, padx=5, pady=5)
-pu_status3.grid(row=8, column=2, padx=5, pady=5)
-pu_status4.grid(row=9, column=2, padx=5, pady=5)
-pu_status5.grid(row=10, column=2, padx=5, pady=5)
-pu_status8.grid(row=11, column=2, padx=5, pady=5)
+pu_statuses = [pu_status0, pu_status1, pu_status2, pu_status3, pu_status4, pu_status5, pu_status8]
+
+for i in range(0,7):
+    pu_statuses[i].grid(row=5+i, column=2, padx=5, pady=5)
 
 # connection and batch infos
-server_address = tk.Entry(master=window, text="server ip", width=20)
+# server_address = tk.Entry(master=window, text="server ip", width=20)
 button_connect = tk.Button(master=window, text="Connect", width=10, command=lambda: pu_client.OPCConnect(x))
 button_disconnect = tk.Button(master=window, text="Disconnect", width=10, command=lambda: pu_client.OPCDisconnect(x))
 
 batch_label = tk.Label(window, text="Batch ID")
 recipe_label = tk.Label(window, text="Recipe ID")
-batch_id = tk.Entry(master=window, text="batch id", width=40)
-recipe_id = tk.Entry(master=window, text="recipe id", width=40)
+batch_id = tk.Entry(master=window, text="batch id", width=30)
+recipe_id = tk.Entry(master=window, text="recipe id", width=30)
 
-server_address.grid(row=1, column=0, padx=5, pady=10)
+# server_address.grid(row=1, column=0, padx=5, pady=10)
 button_disconnect.grid(row=1, column=2, padx=5, pady=10)
 button_connect.grid(row=1, column=1, padx=5, pady=10)
 batch_id.grid(row=2, column=1, padx=5, pady=5, columnspan=2)
 recipe_id.grid(row=3, column=1, padx=5, pady=5, columnspan=2)
 batch_label.grid(row=2, column=0, padx=5, pady=10)
 recipe_label.grid(row=3, column=0, padx=5, pady=10)
+
+###################################################### GUI END #############################################################
+
+# thread function to process in parallel opc tags
+x = threading.Thread(target=thread_function, args=("reading pu_status",pu_client, mes_statuses, pu_statuses, batch_id, recipe_id))
 
 window.mainloop()
